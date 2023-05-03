76cd8930afdc329a5b80d584d1399c18defd7ec4
kamada-maki <ikam.adamak17@gmail.com>
2023-05-03 09:19:13 +0900

    p4-6 静的型付けのメリット(2)

--- 01_Introduction/1.1_aboutTypeScript.ts
+++ 01_Introduction/1.1_aboutTypeScript.ts
@@ -9,3 +9,8 @@ function repeatHello(count: number): string {
 console.log(repeatHello);
 //↓型エラーが消える
 console.log(10);
+
+// 1.1.4 静的型付けのメリット(2) ドキュメントと入力補完
+function repeatHello2(count: number): string {
+  return "Hello".repeat(count);
+}
