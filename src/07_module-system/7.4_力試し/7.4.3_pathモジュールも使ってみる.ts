cd58615c9c61af37ef09d2cfa565bbde391ba55d
kamada-maki <ikam.adamak17@gmail.com>
2023-05-28 10:06:32 +0900

    p335-343 npm/node_mofule/力試し

---
+++ src/07_module-system/7.4_力試し/7.4.3_pathモジュールも使ってみる.ts
@@ -0,0 +1,10 @@
+console.log(import.meta.url);
+import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const filePath = fileURLToPath(import.meta.url);
+const fileDir = path.dirname(filePath);
+const dataFile = path.join(fileDir, "../uhyo.txt");
+
+const data = readFileSync(dataFile, { encoding: "utf-8" });
