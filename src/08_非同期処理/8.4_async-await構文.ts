e7c86291c0e1e10ee567ab3d423c9365187d51fb
kamada-maki <ikam.adamak17@gmail.com>
2023-06-04 10:49:16 +0900

    p376-382 await構文

--- src/08_非同期処理/8.4_async-await構文.ts
+++ src/08_非同期処理/8.4_async-await構文.ts
@@ -1,3 +1,5 @@
+import { readFile, writeFile } from "fs/promises";
+
 //8.4.1 async関数を作ってみる
 export const sentence1 = () => {
   async function get3(): Promise<number> {
@@ -34,7 +36,113 @@ export const sentence2 = () => {
   //   return 3;
   // }
 };
-export const sentence4 = () => {};
-export const sentence5 = () => {};
-export const sentence6 = () => {};
-export const sentence7 = () => {};
+
+//8.4.2 await式も使ってみる
+export const sentence4 = () => {
+  const sleep = (duration: number) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(resolve, duration);
+    });
+  };
+  async function get3() {
+    await sleep(1000);
+    return 3;
+  }
+  const p = get3();
+  p.then((num) => {
+    console.log(`num is ${num}`);
+  });
+};
+export const sentence5 = () => {
+  const sleep = (duration: number) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(resolve, duration);
+    });
+  };
+  async function get3() {
+    console.log("get3が呼び出されました");
+    await sleep(1000);
+    console.log("awaitの次に進みました");
+    return 3;
+  }
+  console.log("get3を呼び出します");
+  const p = get3();
+  p.then((num) => {
+    console.log(`num is ${num}`);
+  });
+  console.log("get3を呼び出しました");
+};
+
+// 8.4.3 awaitの返り値
+export const sentence6 = () => {
+  const sleep = (duration: number) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(resolve, duration);
+    });
+  };
+
+  async function get3() {
+    await sleep(1000);
+    return 3;
+  }
+  async function main() {
+    const num1 = await get3();
+    const num2 = await get3();
+    const num3 = await get3();
+    return num1 + num2 + num3;
+  }
+  main().then((result) => {
+    console.log(`result is ${result}`);
+  });
+};
+export const sentence7 = () => {
+  async function main() {
+    const { readFile, writeFile } = await import("fs/promises");
+    const fooContent = await readFile("foo.txt", "utf8");
+
+    //2倍にしてbar.txtに書き込む
+    await writeFile("bar.txt", fooContent + fooContent);
+    console.log("書き込み完了しました");
+  }
+  main().then(() => {
+    console.log("main()が完了しました");
+  });
+};
+
+//8.4.4 awaitとエラー処理
+export const sentence8 = () => {
+  async function main() {
+    const { readFile, writeFile } = await import("fs/promises");
+
+    try {
+      const fooContent = await readFile("foo.txt", "utf8");
+      //2倍にしてbar.txtに書き込む
+      await writeFile("bar.txt", fooContent + fooContent);
+      console.log("書き込み完了しました");
+    } catch {
+      console.log("失敗しました");
+    }
+  }
+
+  main().then(() => {
+    console.log("main()が成功しました");
+  });
+};
+
+// 8.4.5 async関数のいろいろな宣言方法
+export const sentence9 = () => {
+  const main = async function () {
+    const fooContent = await readFile("foo.txt", "utf8");
+
+    // 2倍にしてbar.txtに書き込む
+    await writeFile("bar.txt", fooContent + fooContent);
+    console.log("書き込みが完了しました");
+  };
+
+  const main2 = async () => {
+    const fooContent = await readFile("foo.txt", "utf8");
+    //2倍にしてbar.txtに書き込む
+    await writeFile("bar.txt", fooContent + fooContent);
+    console.log("書き込み完了しました");
+  };
+};
